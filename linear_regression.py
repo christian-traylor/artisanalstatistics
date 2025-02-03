5f22f643a68e5d2303a136d5d9b4dfbdf38d9cd8
christian-traylor <christianrtraylor@gmail.com>
2025-02-02 20:00:57 -0600

    Solve via analytical solution instead of g.d.

--- linear_regression.py
+++ linear_regression.py
@@ -18,73 +18,25 @@ if plot_data:
     plt.title(" ")
     plt.show()
 
-# Problem statement
-# We want to see if weight is a predictor of height, so weight is our independent variable.
-# We use R^2 to determine blah blah blah
-# R^2:  SSR(mean) - SSR(fitted line)
-#       ____________________________
-#                SSR(mean)
-#
-# SSR = (observed_i - predicted_i)^2
-#
-# 1. Calculate SSR(fitted line):
-# * Calculate maximum and minimum Y coordinates, this will inform us when to stop in training.
-# ** Take the absolute highest or lowest Y coordinate and set the y_intercept to that
-# ** Training will be limited to [Y_lowest, Y_highest]
+# Problem statement:
+# Determine if mouse weight is a predictor of mouse height.
+# -- Find the analytical solution via minimizing Y = B_0 + B_1 * X.
+# -- The parameters B_0 and B_1 can be found with the following equations:
+# -- B_1 = sum((X_i - X_bar) * (Y_i - Y_bar))
+# --       ________________________________
+# --              sum((X_i - X_bar)^2)
+# -- 
+# --
+# -- B_0 = Y_bar - (B_1 * X_bar) 
 
+X_bar = 0
+Y_bar = 0
 
-fitted_y_intercept = 0
-fitted_slope = 1
-mean_intercept = 0
-MEAN_SLOPE = 0
-data_y_lowest = inf
-data_y_highest = -inf
+for x, y in data:
+    X_bar += x
+    Y_bar += y
 
-for _, y in data:
-    data_y_lowest = min(data_y_lowest, y)
-    data_y_highest = max(data_y_highest, y)
+X_bar /= len(data)
+Y_bar /= len(data)
 
-
-fitted_y_intercept = None
-direction = None
-if abs(data_y_lowest) > abs(data_y_highest):
-    fitted_y_intercept = data_y_lowest
-    direction = "UP"
-else:
-    fitted_y_intercept = data_y_highest
-    direction = "DOWN"
-
-
-# * Minimize RSS w.r.t. fitted_y_intercept using the equation:
-# * y = mx + b
-rss_y_intercept_loss_best_value = inf
-rss_y_intercept_loss_best_value_number = None
-
-# We should maybe just sort the data, so we can adjust move to the next data point. Set fitted_y_intercept to next data point
-
-for x, _ in data:
-    if direction == "DOWN":
-        fitted_y_intercept = None # we need variation 
-    for x_i, observed_i in data:
-        predicted_i = (x_i * fitted_slope) + fitted_y_intercept
-        rss_y_intercept_loss = (observed_i - predicted_i)**2
-        if rss_y_intercept_loss < rss_y_intercept_loss_best_value:
-            rss_y_intercept_loss_best_value_number = fitted_y_intercept
-            rss_y_intercept_loss_best_value = rss_y_intercept_loss
-
-print(rss_y_intercept_loss_best_value_number)
-
-# * Minimize RSS w.r.t. fitted_slope using the equation:
-# * y = mx + b
-# rss_slope_loss_best_value = -1
-# rss_slope_loss_best_value_number = None
-
-# for x, _ in data: 
-#     for x_i, observed_i in data:
-#         predicted_i = (x_i * fitted_slope) + fitted_y_intercept
-#         rss_slope_loss = (observed_i - predicted_i)**2
-#         if rss_y_intercept_loss < rss_y_intercept_loss_best_value:
-#             rss_slope_loss_best_value_number = fitted_y_intercept
-#             rss_slope_loss_best_value = rss_y_intercept_loss
-
-# 2. Calculate SSR(mean):
+print(X_bar, Y_bar)
